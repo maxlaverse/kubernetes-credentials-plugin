ec0b754e64f7be342e07ab90b2ece92d17be2d0a
Carlos Sanchez <carlos@apache.org>
2018-01-23 18:19:41 +0100

    More meaningful credential names
    They apply to kubernetes, not just openshift
    
    OpenShiftTokenCredentialImpl is not needed as there is StringCredentialsImpl

--- src/main/java/org/jenkinsci/plugins/kubernetes/credentials/FileSystemServiceAccountCredential.java
+++ src/main/java/org/jenkinsci/plugins/kubernetes/credentials/FileSystemServiceAccountCredential.java
@@ -1,15 +1,18 @@
 package org.jenkinsci.plugins.kubernetes.credentials;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+import org.kohsuke.stapler.DataBoundConstructor;
+
 import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.impl.BaseStandardCredentials;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.Extension;
-import org.apache.commons.io.FileUtils;
-import org.kohsuke.stapler.DataBoundConstructor;
-
-import java.io.File;
-import java.io.IOException;
+import hudson.util.Secret;
 
 /**
  * Read the OAuth bearer token from service account file provisioned by kubernetes
@@ -18,23 +21,33 @@ import java.io.IOException;
  *
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
-public class ServiceAccountCredential extends BaseStandardCredentials implements TokenProducer {
+public class FileSystemServiceAccountCredential extends BaseStandardCredentials
+        implements TokenProducer, StringCredentials {
+
+    private static final long serialVersionUID = -2222994164073756041L;
 
     private static final String SERVICEACCOUNT_TOKEN_PATH = "/var/run/secrets/kubernetes.io/serviceaccount/token";
 
     @DataBoundConstructor
-    public ServiceAccountCredential(CredentialsScope scope, String id, String description) {
+    public FileSystemServiceAccountCredential(CredentialsScope scope, String id, String description) {
         super(scope, id, description);
     }
 
     @Override
     @SuppressFBWarnings("DMI_HARDCODED_ABSOLUTE_FILENAME")
-    public String getToken(String serviceAddress, String caCertData, boolean skipTlsVerify) {
+    public Secret getSecret() {
         try {
-            return FileUtils.readFileToString(new File(SERVICEACCOUNT_TOKEN_PATH));
+            return Secret.fromString(FileUtils.readFileToString(new File(SERVICEACCOUNT_TOKEN_PATH)));
         } catch (IOException e) {
-            return null;
+            return Secret.fromString(null);
         }
+    }
+
+    @Override
+    @SuppressFBWarnings("DMI_HARDCODED_ABSOLUTE_FILENAME")
+    @Deprecated
+    public String getToken(String serviceAddress, String caCertData, boolean skipTlsVerify) {
+        return getSecret().getPlainText();
     }
 
     @Extension(optional = true)
@@ -52,4 +65,5 @@ public class ServiceAccountCredential extends BaseStandardCredentials implements
             return "Kubernetes Service Account";
         }
     }
+
 }
